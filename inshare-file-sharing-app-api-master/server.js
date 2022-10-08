c3d97f16e438e8b2ad0994add8b504de39f138b0
ratnesh123-dubey <95767134+ratnesh123-dubey@users.noreply.github.com>
2022-10-08 10:00:34 +0530

    Update server.js

--- inshare-file-sharing-app-api-master/server.js
+++ inshare-file-sharing-app-api-master/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const path = require('path')
 const connectDB = require('./config/db');
 // connectDB();
